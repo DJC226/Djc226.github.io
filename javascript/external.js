5661fa6bf8cc01284c5925fd80fd7f4f4023f9d1
Djc226 <djc226@txstate.edu>
2025-09-25 01:34:32 -0500

    “Completed Task 4 Operators

--- javascript/external.js
+++ javascript/external.js
@@ -10,5 +10,13 @@
 // const out = x + y;
 // document.write(out);
 
-user_name = window.prompt("Please enter your name", "Type your name here");
-document.write(user_name);
+// user_name = window.prompt("Please enter your name", "Type your name here");
+// document.write(user_name);
+
+a = 22;
+const b = 33;
+document.writeln(a + b);
+document.writeln("<br>");
+document.writeln(a += b);
+document.writeln("<br>");
+document.writeln(a += b);
